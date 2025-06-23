a5c15c9663209aaf6981d10bb597f3435dd8ddfd
valentincharlone <valencharlone@gmail.com>
2025-06-23 19:36:51 -0300

    Add portfolio components and update styles
    - Introduced new components: Header, Hero, About, Divider, and ImageProfile.
    - Updated layout and styles in layout.tsx and globals.css.
    - Configured ESLint rules for better code quality.
    - Added framer-motion and react-icons dependencies.
    - Included CV and project images in the public directory.

---
+++ components/hero.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import { FaGithubSquare } from "react-icons/fa";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import Link from "next/link";
+import ImageProfile from "./image-profile";
+
+export default function Hero() {
+  return (
+    <section
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <ImageProfile />
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Valentín.</span> I'm a{" "}
+        <span className="font-bold">Frontend developer</span> with{" "}
+        <span className="font-bold">3 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React & (Next.js)</span>.
+      </motion.h1>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack"
+          href="https://linkedin.com/in/valentin-charlone"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
+          href="https://github.com/valentincharlone"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
---
+++ components/image-profile.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function ImageProfile() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="relative">
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "tween",
+            duration: 0.2,
+          }}
+        >
+          <Image
+            alt="valentin charlone"
+            src="/me.jpg"
+            width={192}
+            height={192}
+            quality={95}
+            priority
+            className="h-32 w-32 rounded-full object-cover border-[0.20rem] border-white shadow-xl"
+          />
+        </motion.div>
+        <motion.span
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 125,
+            delay: 0.1,
+            duration: 0.7,
+          }}
+          className="absolute bottom-0 -right-3 text-4xl"
+        >
+          👋
+        </motion.span>
+      </div>
+    </div>
+  );
+}

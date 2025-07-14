9bf817598a719f3ada0cdbf856263725245b8dfe
valentincharlone <valencharlone@gmail.com>
2025-07-14 09:59:30 -0300

    Add Contact and Footer components; update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import About from "@/components/about";
+import Contact from "@/components/contact";
 import Divider from "@/components/divider";
 import Experience from "@/components/experience";
+import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
@@ -14,6 +16,8 @@ export default function Home() {
       <Projects />
       <Skills />
       <Experience />
+      <Contact />
+      <Footer />
     </main>
   );
 }
--- components/experience.tsx
+++ components/experience.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import { Fragment } from "react";
 import SectionHeading from "./section-heading";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
-import { experiencesData } from "@/lib/data";
+
+// import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function Experience() {
@@ -16,40 +11,6 @@ export default function Experience() {
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My Experience</SectionHeading>
-      <VerticalTimeline animate={true} lineColor="#e5e7eb">
-        {experiencesData.map((item, index) => (
-          <Fragment key={index}>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              contentStyle={{
-                background: "#f3f4f6",
-                boxShadow: "none",
-                border: "1px solid rgba(0, 0, 0, 0.05)",
-                textAlign: "left",
-                padding: "1.3rem 2rem",
-              }}
-              contentArrowStyle={{
-                borderRight: "0.4rem solid #9ca3af",
-              }}
-              date={item.date}
-              icon={item.icon}
-              iconStyle={{
-                background: "white",
-                fontSize: "1.5rem",
-                color: "#374151",
-                boxShadow:
-                  "0 0 0 4px #e5e7eb, inset 0 2px 0 rgba(0,0,0,.08), 0 3px 0 4px rgba(0,0,0,.05)",
-              }}
-            >
-              <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="font-normal !mt-0">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700">
-                {item.description}
-              </p>
-            </VerticalTimelineElement>
-          </Fragment>
-        ))}
-      </VerticalTimeline>
     </section>
   );
 }

99cf8397abaf9edc88cd090cda5abefd128e98c4
valentincharlone <valencharlone@gmail.com>
2025-07-17 12:28:22 -0300

    feat: update project data; modify tags and descriptions for CorpComment, rmtDev, and Pet Soft

--- lib/data.ts
+++ lib/data.ts
@@ -82,21 +82,29 @@ export const projectsData = [
     title: "CorpComment",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    tags: ["React", "TypeScript", "Zustand", "CSS", "Vite", "Vercel"],
     imageUrl: corpcommentImg,
   },
   {
     title: "rmtDev",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    tags: ["React", "TypeScript", "React-Query", "CSS", "Vite", "Vercel"],
     imageUrl: rmtdevImg,
   },
   {
-    title: "Word Analytics",
+    title: "Pet Soft",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+      "Software de gestión integral para guarderías caninas. PetSoft simplifica la administración del negocio, permitiendo a los dueños registrar mascotas, gestionar reservas, procesar pagos de forma segura con Stripe y mantener un control total sobre los animales a su cuidado.",
+    tags: [
+      "React",
+      "Next.js",
+      "Tailwind",
+      "Shadcn",
+      "Prisma",
+      "Stripe",
+      "Vercel",
+    ],
     imageUrl: wordanalyticsImg,
   },
 ] as const;
